1f75da544704bd3463b68c2b90fbcd57ebd6957f
Sam Bisbee <sam@sbisbee.com>
2011-03-03 21:06:27 -0500

    Rewriting to use our own data structure instead of duck punching localStorage.

--- couchStorage.js
+++ couchStorage.js
@@ -1,34 +1,36 @@
-(function()
+var couchStorage = (function()
 {
-  if(!window.couchStorage)
-    window.couchStorage = {};
+  var 
+      //our storage device where we, uh, store things
+      dataCylinder = {},
 
-  $.couch.urlPrefix = "/db";
+      //the key to use when writing to and reading from Storage objects
+      BROWSER_STORAGE_KEY = "couchStorage";
 
-  window.couchStorage.start = function()
-  {
-    Storage.prototype._oldSetItem = Storage.prototype.setItem;
-    Storage.prototype._oldGetItem = Storage.prototype.getItem;
-
-    Storage.prototype.setItem = function(key, value)
+  return {
+    /*
+     * Acts just like Storage.setItem, except it also tries to persist the
+     * object into CouchDB using the key as the document's _id.
+     *
+     * Only accepts objects for values.
+     */
+    setItem: function(key, value)
     {
-      var db = $.couch.db("jquery"),
-          currStorage = this;
+      $.couch.urlPrefix = '/db';
+      var db = $.couch.db('jquery');
 
-      if(!value)
+      if(value === null)
       {
-        var toDelete = currStorage.getItem(key);
-
-        if(toDelete)
-          db.removeDoc(toDelete);
+        dataCylinder[key] = null;
 
         return null;
       }
-      else if(typeof value == "object")
+      else if(typeof value == 'object')
       {
         value._id = key;
 
-        var prevLocalCopy = currStorage.getItem(key);
+        var prevLocalCopy = dataCylinder[key];
+
         if(prevLocalCopy && prevLocalCopy._rev)
           value._rev = prevLocalCopy._rev;
 
@@ -37,29 +39,55 @@
           {
             success: function(resp) {
               value._rev = resp.rev;
-              currStorage._oldSetItem(key, JSON.stringify(value)); 
+              dataCylinder[key] = value;
             }
           }
         );
-
-        return value;
       }
+      else
+        throw new Error('Invalid type: value must be an object or null.');
+    },
 
-      return currStorage._oldSetItem(key, value);
-    };
-
-    Storage.prototype.getItem = function(key)
+    /*
+     * Returns the item from our local storage. If it is not found, then we try
+     * to get the item from CouchDB, storing it locally if we succeed.
+     */
+    getItem: function(key)
     {
-      var value = this._oldGetItem(key);
+      if(dataCylinder[key])
+        return dataCylinder[key];
 
-      try
-      {
-        return JSON.parse(value);
-      }
-      catch(e)
-      {
-        return value;
-      }
-    };
-  }
+      //TODO try to get the item from couchdb
+
+      return null;
+    },
+
+    /*
+     * Attempt to copy our internal data structure into the passed Storage
+     * object (ex., localStorage), allowing ourselves to be persisted.
+     */
+    writeTo: function(storage)
+    {
+      if(typeof storage != 'object' || !storage.setItem)
+        throw new Error('Unsupported storage type.');
+
+      storage.setItem(BROWSER_STORAGE_KEY, JSON.stringify(dataCylinder));
+
+      return storage;
+    },
+
+    /*
+     * Attempt to read an internal data structure representation from the
+     * passed Storage object (ex., localStorage).
+     */
+    readFrom: function(storage)
+    {
+      if(typeof storage != 'object' || !storage.getItem)
+        throw new Error('Unsupported storage type.');
+
+      dataCylinder = JSON.parse(storage.getItem(BROWSER_STORAGE_KEY));
+
+      return storage;
+    }
+  };
 })();
